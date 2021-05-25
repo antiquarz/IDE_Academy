5286ec10cc583f0ccd717e53373d4b48318cc9b4
George Cave <cave@hey.com>
2021-05-25 15:00:04 +0100

    Update to 16 LEDs

--- 01_Colour_ring/01_Colour_ring.ino
+++ 01_Colour_ring/01_Colour_ring.ino
@@ -3,7 +3,7 @@
 
 // Setup FastLED pins
 #define DATA_PIN 6
-#define NUM_LEDS 20
+#define NUM_LEDS 16
 
 // Debouncer for button
 #define PUSH_BUTTON 7
--- 02_Colour_gradient/02_Colour_gradient.ino
+++ 02_Colour_gradient/02_Colour_gradient.ino
@@ -3,7 +3,7 @@
 
 // Setup FastLED pins
 #define DATA_PIN 6
-#define NUM_LEDS 20
+#define NUM_LEDS 16
 
 // Pin for rotary button
 #define ROTARY_BUTTON A0
